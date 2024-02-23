da48f160a1aacc4f39bd2bdaf9c0494457850fc6
flam1ngo <kris.s.03@mail.ru>
2024-02-23 22:08:41 +0300

    Initial Commit

---
+++ TryCatchTask1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TryCatchTask1
+//
+//  Created by Кристина Скворцова on 23.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
