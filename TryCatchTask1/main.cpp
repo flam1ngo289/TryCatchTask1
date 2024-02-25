d00b40dc1bc403813708c367d8d2b9a68e80d07d
flam1ngo <kris.s.03@mail.ru>
2024-02-25 19:28:49 +0300

    Initial commit

--- TryCatchTask1/main.cpp
+++ TryCatchTask1/main.cpp
@@ -1,14 +1,35 @@
-//
-//  main.cpp
-//  TryCatchTask1
-//
-//  Created by Кристина Скворцова on 23.02.2024.
-//
-
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class bad_length {}; 
+
+long function(std::string &str, int forbidden_length) {
+    long length = str.length();
+    if ((length / 2) == forbidden_length) {
+        throw bad_length();
+    }
+    return length;
+}
+
+int main() {
+    int forbidden_length;
+    std::cout << "Введите запретную длину: ";
+    std::cin >> forbidden_length;
+
+    std::string input;
+    
+    while (true) {
+        std::cout << "Введите слово: ";
+        std::cin >> input;
+
+        try {
+            long len = function(input, forbidden_length);
+            std::cout << "Длина слова \"" << input << "\" равна " << len / 2 << std::endl;
+        } catch (bad_length) {
+            std::cout << "Вы ввели слово запретной длины! До свидания" << std::endl;
+            break;
+        }
+    }
+
     return 0;
 }
